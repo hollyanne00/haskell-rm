a965d136d4f5b9a5909429d05cfc449151bc3f35
Holly <hollyanne00@googlemail.com>
2014-01-24 17:37:06 +0000

    added basic functions to run a program defined as a series of instructions

--- util.hs
+++ util.hs
@@ -1,5 +1,6 @@
--- Define register, label and instruction
+-- Define instructions as Halt, Ri+=>Lj or Ri-=>Lj,Lk 
 data Instruct = Rplus Int Int | Rminus Int Int Int | Halt
+-- Define program as a list of instructions
 
 -- Compute fat brackets
 fatbr (x,y) = 2^x * (2*y + 1)
@@ -20,3 +21,10 @@ codeinst (Rminus i j k) = fatbr(2*i + 1, thinbr (j, k))
 codeilist [] = 0
 codeilist (x:xs) = fatbr (codeinst x, codeilist xs)
 
+-- Execute and instruction, returning updated program counter and registers
+execute (Halt) rs  pc = (rs, pc)
+execute (Rplus i j) rs pc = let r = (head (take 1 (drop (i-1) rs))) in ((take (i-1) rs) ++ (r+1) : (drop 1 rs), j)
+execute (Rminus i j k) rs pc = let r = (head (take 1 (drop (i-1) rs)))-1 in ((take (i-1) rs) ++ (if r<0 then 0 else r) : (drop 1 rs), (if r>=0 then j else k))
+
+-- Run a program
+run (x:xs) rs = let (nrs,pc) = execute x rs 0 in execute ((x:xs) !! pc) nrs pc 
